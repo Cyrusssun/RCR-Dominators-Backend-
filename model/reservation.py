84939787e37ab8364f7d6d2de1a85bd349af6e6f
cyrusun-0505 <cyrusun0505@gmail.com>
2026-03-13 13:46:27 -0700

    fix

--- model/reservation.py
+++ model/reservation.py
@@ -4,7 +4,7 @@ import random, string
 
 import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from __init__ import db
+from __init__ import app, db
 
 
 class Reservation(db.Model):
